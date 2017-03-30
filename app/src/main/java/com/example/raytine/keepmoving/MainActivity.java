31530cc02303a4cf68ea24c8a626783dd3966c4f
shaozhengwang <956624562@qq.com>
2017-03-30 19:05:05 +0800

    guide design

--- app/src/main/java/com/example/raytine/keepmoving/MainActivity.java
+++ app/src/main/java/com/example/raytine/keepmoving/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.raytine.keepmoving;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT > 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
+
 }
